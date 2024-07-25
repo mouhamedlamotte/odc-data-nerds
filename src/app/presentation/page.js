b72f9f66985d7ace8cf99b3f707692b8c5697545
mouhamedbaba <lamottelymouhamed@gmail.com>
2024-07-25 22:52:01 +0000

    fix : delete styles froms presentation iframe

--- src/app/presentation/page.js
+++ src/app/presentation/page.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+// "width: 700px; max-width: 100%; height: 450px" 
 const PresentationPage = () => {
   return (
     <div className='h-screen w-full flex justify-center items-center'>
-<iframe src="https://gamma.app/embed/rnszwaeejeq5str" style="width: 700px; max-width: 100%; height: 450px" allow="fullscreen" title="Firebase : Une Plateforme Cloud Puissante"></iframe>
+<iframe src="https://gamma.app/embed/rnszwaeejeq5str" className='w-full h-full' allow="fullscreen" title="Firebase : Une Plateforme Cloud Puissante"></iframe>
     </div>
   )
 }
