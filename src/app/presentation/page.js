bfa1256736be5401467fee704a88ee6e6132c7df
mouhamedbaba <lamottelymouhamed@gmail.com>
2024-07-25 22:49:21 +0000

    feat : add gama firestore prsentation page

---
+++ src/app/presentation/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PresentationPage = () => {
+  return (
+    <div className='h-screen w-full flex justify-center items-center'>
+<iframe src="https://gamma.app/embed/rnszwaeejeq5str" style="width: 700px; max-width: 100%; height: 450px" allow="fullscreen" title="Firebase : Une Plateforme Cloud Puissante"></iframe>
+    </div>
+  )
+}
+
+export default PresentationPage
